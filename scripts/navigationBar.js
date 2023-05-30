7a5f0df0ef0f1071a08ade4ed5525be02681115f
Adharsh Reghu <adharshreghu2002@gmail.com>
2023-05-30 19:45:17 +0530

    added qr generator

--- scripts/navigationBar.js
+++ scripts/navigationBar.js
@@ -1,9 +1,12 @@
+//script for drop-down of hamburger menu in small screens
 const toggleButton = document.querySelector('.toggle-button');
 const navbarLinks = document.querySelector('.navbar-links');
 toggleButton.addEventListener('click',()=>{
     navbarLinks.classList.toggle('activeNav');
 })
 
+
+//script for changing background color of content section of the home page 
 const contentBody = document.querySelector('.content');
 const logoButton = document.querySelector('.logo');
 console.log(logoButton);
---
+++ scripts/qrg.js
@@ -0,0 +1,46 @@
+//script for drop-down of hamburger menu in small screens
+const toggleButton = document.querySelector('.toggle-button');
+const navbarLinks = document.querySelector('.navbar-links');
+toggleButton.addEventListener('click',()=>{
+    navbarLinks.classList.toggle('activeNav');
+})
+
+
+//script for changing background color of content section of the home page 
+const contentBody = document.querySelector('.body');
+const logoButton = document.querySelector('.logo');
+console.log(logoButton);
+logoButton.addEventListener('click',()=>{
+    contentBody.classList.toggle('darkmode');
+})
+
+//qr code script
+const container = document.querySelector('.container');
+const btn = container.querySelector('button');
+const input = container.querySelector('form input');
+const image = container.querySelector('.Qr img');
+const qrsize='200x200'
+const url = 'https://api.qrserver.com/v1/create-qr-code/';
+async function getImage (e){
+    e.preventDefault();
+    const inputValue = input.value;
+    if(!inputValue){
+        return
+    }
+    btn.innerText='Generating QR Code....';
+    const response =  await fetch(`${url}?data=${inputValue}&size=${qrsize}`);
+    const source = response.url
+    image.src= source;
+    
+    image.addEventListener('load',()=>{
+        btn.innerText='Generate QR Code';
+        container.classList.add('active')
+    })
+}
+btn.addEventListener('click',getImage);
+
+input.addEventListener('keyup',()=>{
+    if(!input.value){
+        container.classList.remove('active');
+    }
+})
