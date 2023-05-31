8e67ae316194935cdea754fc83fe79510aa7456b
Adharsh Reghu <adharshreghu2002@gmail.com>
2023-06-01 00:54:24 +0530

    image layout created , sample images added, image section is now responsive with customized navigation bar added

---
+++ scripts/images.js
@@ -0,0 +1,18 @@
+//script for drop-down of hamburger menu in small screens
+const toggleButton = document.querySelector('.toggle-button');
+const navbarLinks = document.querySelector('.navbar-links');
+toggleButton.addEventListener('click',()=>{
+    navbarLinks.classList.toggle('activeNav');
+})
+
+
+//script for changing background color of content section of the QR generator page 
+const contentBody = document.querySelector('body');
+const logoButton = document.querySelector('.logo');
+console.log(logoButton);
+logoButton.addEventListener('click',()=>{
+    contentBody.classList.toggle('darkmode');
+})
+
+
+//use pexels api for images
