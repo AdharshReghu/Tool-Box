4d8b52e5d9311d827e52a778903cb50064a5a729
Adharsh Reghu <adharshreghu2002@gmail.com>
2023-06-03 21:38:59 +0530

    next and previous buttons working properly and changind videos

--- scripts/videos.js
+++ scripts/videos.js
@@ -18,7 +18,7 @@ const url = 'https://pixabay.com/api/videos/';
 
 const input = document.querySelector('input');
 const searchButton = document.querySelector('.search');
-searchButton.addEventListener('click',getVideo) 
+
 const nextButton = document.querySelector('.next'); 
 const previousButton = document.querySelector('.previous');
 const videoContainer = document.querySelector('.videoContainer');
@@ -26,33 +26,51 @@ const videoResultBox = document.querySelector('.videoResultBox');
 
 const videos = document.querySelector('.videos')
 var i = 0;
+var numberOfVideos = 0;
 
 var videoArray = undefined; 
+
+searchButton.addEventListener('click',getVideo) 
 
 async function getVideo(e){
     e.preventDefault();
     let inputValue = input.value;
+    i=0;
+    numberOfVideos=0;
+    videoArray=undefined;
     if(!inputValue)
     {
         console.log('input value is empty')
         videoResultBox.classList.remove('videoActive');
         return;
     }
-    videoResultBox.classList.add('videoActive');
     const response = await fetch(`${url}?key=${api_key}&q=${inputValue}`)
     const data = await response.json();
     videoArray = data.hits;
-    const numberOfVideos =  videoArray.length;
+    numberOfVideos =  videoArray.length;
+    if(numberOfVideos===0)
+    {
+        videoResultBox.classList.remove('videoActive');
+        return;
+    }
+    previousButton.disabled=true;
+    nextButton.disabled=false;
+    previousButton.classList.add('disabledButtonColor');
+    if(numberOfVideos===1)
+    {
+        nextButton.disabled=true;
+        nextButton.classList.add('disabledButtonColor');
+    }
+    videoResultBox.classList.add('videoActive');
     let src =  videoArray[0].videos.medium.url;
-    console.log(src);
     let source = document.createElement('source');
     source.src= src;
     source.type='video/mp4';
     source.class = 'video';
-    console.log(source);
-    videos.append(source); 
+    videos.replaceChildren();
+    videos.append(source);
+    videos.load();
 }
-console.log('hello');
 
 input.addEventListener('keyup',()=>{
     if(!input.value)
@@ -60,3 +78,58 @@ input.addEventListener('keyup',()=>{
         videoResultBox.classList.remove('videoActive');
     }
 })
+
+nextButton.addEventListener('click',nextVideo);
+
+function nextVideo(){
+    
+    let source = document.createElement('source');
+    console.log(i);
+    console.log(numberOfVideos);
+    videos.replaceChildren();
+    if(i<numberOfVideos-1)
+    {
+        previousButton.disabled=false;
+        previousButton.classList.remove('disabledButtonColor');
+        i++;
+        source.src=videoArray[i].videos.medium.url;
+        source.type='video/mp4';
+        source.class = 'video';
+        console.log(source);
+        videos.append(source);
+        videos.load();
+        if(i=numberOfVideos-1)
+        {
+            nextButton.disabled=true;
+            nextButton.classList.add('disabledButtonColor');
+        }
+    }
+
+}
+
+previousButton.addEventListener('click',prevVideo);
+
+function prevVideo(){
+   
+    let source = document.createElement('source');
+    console.log(i);
+    console.log(numberOfVideos);
+    videos.replaceChildren();
+    if(i>0)
+    {
+        nextButton.disabled=false;
+        nextButton.classList.remove('disabledButtonColor');
+        i--;
+        source.src=videoArray[i].videos.medium.url;
+        source.type='video/mp4';
+        source.class = 'video';
+        console.log(source);
+        videos.append(source);
+        videos.load();
+        if(i<=0)
+        {
+            previousButton.disabled=true;
+            previousButton.classList.add('disabledButtonColor');
+        }
+    }
+}
