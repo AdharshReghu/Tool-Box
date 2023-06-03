44dbf2333a1e216442969fd70893fadc6358b2b4
Adharsh Reghu <adharshreghu2002@gmail.com>
2023-06-03 16:36:06 +0530

    videos pulled through api and first video is displayed on the screen

--- scripts/videos.js
+++ scripts/videos.js
@@ -15,3 +15,48 @@ logoButton.addEventListener('click',()=>{
 
 const api_key = '36981079-6cd34e73acd18301edddeec1d';
 const url = 'https://pixabay.com/api/videos/';
+
+const input = document.querySelector('input');
+const searchButton = document.querySelector('.search');
+searchButton.addEventListener('click',getVideo) 
+const nextButton = document.querySelector('.next'); 
+const previousButton = document.querySelector('.previous');
+const videoContainer = document.querySelector('.videoContainer');
+const videoResultBox = document.querySelector('.videoResultBox');
+
+const videos = document.querySelector('.videos')
+var i = 0;
+
+var videoArray = undefined; 
+
+async function getVideo(e){
+    e.preventDefault();
+    let inputValue = input.value;
+    if(!inputValue)
+    {
+        console.log('input value is empty')
+        videoResultBox.classList.remove('videoActive');
+        return;
+    }
+    videoResultBox.classList.add('videoActive');
+    const response = await fetch(`${url}?key=${api_key}&q=${inputValue}`)
+    const data = await response.json();
+    videoArray = data.hits;
+    const numberOfVideos =  videoArray.length;
+    let src =  videoArray[0].videos.medium.url;
+    console.log(src);
+    let source = document.createElement('source');
+    source.src= src;
+    source.type='video/mp4';
+    source.class = 'video';
+    console.log(source);
+    videos.append(source); 
+}
+console.log('hello');
+
+input.addEventListener('keyup',()=>{
+    if(!input.value)
+    {
+        videoResultBox.classList.remove('videoActive');
+    }
+})
